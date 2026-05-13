e95adec0445431e7753f1ef71a617ba652010349
Alexander Robinson <alexander.robinson@awi.de>
2026-05-13 06:56:59 +0200

    velocity_diva: minor bug fix.

--- src/physics/velocity_diva.f90
+++ src/physics/velocity_diva.f90
@@ -1035,6 +1035,9 @@ end if
         else 
             ! Calculate basal velocity normally 
 
+            ! Set boundary condition code
+            BC = boundary_code(boundaries)
+
             !$omp parallel do collapse(2) private(i,j,im1,ip1,jm1,jp1,F2_ac)
             do j = 1, ny 
             do i = 1, nx 
