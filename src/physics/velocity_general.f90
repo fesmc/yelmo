13b7a30dfa69ef3b056fad7b9b355c8987613423
Alexander Robinson <alexander.robinson@awi.de>
2026-07-08 12:35:17 +0200

    vel: make taud_gl_method=-1 a hard error stop instead of silent no-op

--- src/physics/velocity_general.f90
+++ src/physics/velocity_general.f90
@@ -1185,75 +1185,10 @@ end if
         select case(method)
 
             case(-1)
-                ! One-sided choice
-                ! between upstream or downstream driving stress 
-                ! at grounding line
+                ! Unsupported option: hard failure
 
-                if (beta_gl_stag .eq. 1) then 
-                    ! Upstream beta assigned at gl (ie, beta=beta_upstream)
-
-if (.FALSE.) then 
-                    ! x-direction 
-                    do j = 1, ny 
-                    do i = 2, nx-1 
-
-                        if (f_grnd(i,j) .eq. 0.0 .and. f_grnd(i+1,j) .gt. 0.0) then 
-                            taud_acx(i,j) = taud_acx(i+1,j) 
-                        else if (f_grnd(i,j) .gt. 0.0 .and. f_grnd(i+1,j) .eq. 0.0) then  
-                            taud_acx(i,j) = taud_acx(i-1,j)
-                        end if 
-
-                    end do 
-                    end do 
-
-                    ! y-direction 
-                    do j = 2, ny-1 
-                    do i = 1, nx 
-
-                        if (f_grnd(i,j) .eq. 0.0 .and. f_grnd(i,j+1) .gt. 0.0) then 
-                            taud_acy(i,j) = taud_acy(i,j+1) 
-                        else if (f_grnd(i,j) .gt. 0.0 .and. f_grnd(i,j+1) .eq. 0.0) then  
-                            taud_acy(i,j) = taud_acy(i,j-1)
-                        end if 
-
-                    end do 
-                    end do 
-end if 
-                else if (beta_gl_stag .eq. 2) then 
-                    ! Downstream beta assigned at gl (ie, beta=0)
-
-                    ! x-direction 
-                    do j = 1, ny 
-                    do i = 2, nx-1 
-
-                        if (f_grnd(i,j) .eq. 0.0 .and. f_grnd(i+1,j) .gt. 0.0) then 
-                            taud_acx(i,j) = taud_acx(i-1,j) 
-                        else if (f_grnd(i,j) .gt. 0.0 .and. f_grnd(i+1,j) .eq. 0.0) then  
-                            taud_acx(i,j) = taud_acx(i+1,j)
-                        end if 
-
-                    end do 
-                    end do 
-
-                    ! y-direction 
-                    do j = 2, ny-1 
-                    do i = 1, nx 
-
-                        if (f_grnd(i,j) .eq. 0.0 .and. f_grnd(i,j+1) .gt. 0.0) then 
-                            taud_acy(i,j) = taud_acy(i,j-1) 
-                        else if (f_grnd(i,j) .gt. 0.0 .and. f_grnd(i,j+1) .eq. 0.0) then  
-                            taud_acy(i,j) = taud_acy(i,j+1)
-                        end if 
-
-                    end do 
-                    end do 
-
-                else 
-
-                    write(*,*) "calc_driving_stress_gl:: Error: Wrong choice of beta_gl_stag for this method."
-                    stop 
-
-                end if 
+                write(*,*) "calc_driving_stress_gl:: Error: taud_gl_method=-1 not supported."
+                error stop "taud_gl_method=-1 not supported"
 
             case(1)
                 ! Weighted average using the grounded fraction (ac-nodes)
